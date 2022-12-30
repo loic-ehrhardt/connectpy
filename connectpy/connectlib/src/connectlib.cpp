532fc3099c977bc103110c71f7a22204b93abaf1
Loïc Ehrhardt <loic.ehrhardt@gmail.com>
2022-12-30 15:31:29 +0000

    Use pybind11 in connectlib.

--- connectpy/connectlib/src/connectlib.cpp
+++ connectpy/connectlib/src/connectlib.cpp
@@ -1,6 +1,18 @@
+#include <pybind11/pybind11.h>
 #include <iostream>
+
+namespace py = pybind11;
 
 int main() {
     std::cout << "Hello world" << std::endl;
     return 0;
+}
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(connectlib, m) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+    m.def("add", &add, "A function that adds two numbers");
 }
